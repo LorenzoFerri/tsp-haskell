c9edcba845621b453353f9cf684e9b04ac43997d
Lorenzo Ferri <lorenzo.ferri@usi.ch>
2018-07-13 17:38:32 +0200

    trying random numbers

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,8 @@ module Main where
 import Parse
 import System.Environment  (getArgs)
 import Text.Parsec
+import Definitions
+import System.Random
 
 main :: IO ()
 main = do
@@ -10,9 +12,15 @@ main = do
   if null args then
     putStrLn "No args"
   else do
-    input <- readFile $ head args
-    case parse parseInput (head args) input of
-      Right s -> do
-        putStrLn $ show s
+    let file = head args
+    let seed = read $ head $ tail args
+    let gen = mkStdGen seed
+    input <- readFile $ file
+    case parse parseInput file input of
+      Right distanceMatrix -> do
+        randomRIO (3,999)
       Left err ->
         putStrLn $ show err
+
+-- createGenerator [] = mkStdGen (take 1 (randoms newStdGen))
+-- createGenerator seed = 
